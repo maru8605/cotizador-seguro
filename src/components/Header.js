c6a94612440b635c3387cda26f1df69973a1edd5
maru8605 <maru8605@hotmail.com>
2021-07-26 20:42:55 -0300

    header + emotion

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+const HeaderContainer = styled.header`
+   background-color: #26C6DA;
+   padding: 10px;
+   font-weight: bold;
+   color: ffffff;
+`
+const HeaderText = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
+
+export const Header = ({titulo}) => {
+    return (
+        <HeaderContainer>
+            <HeaderText>{titulo}</HeaderText>
+        </HeaderContainer>
+    )
+}
+export default Header;
